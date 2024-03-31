c8aa936cbe9cc901d2a3a359b626e12a14eda9ba
HoangTran <jonny.tran12902@gmail.com>
2024-03-31 23:08:29 +0700

    update dataloader

--- link_prediction/dataloaders/hive.py
+++ link_prediction/dataloaders/hive.py
@@ -6,84 +6,77 @@ from torch_geometric.data import HeteroData
 
 
 def getHiveDataset(
-    node_file,
-    user_link_edge_file,
-    user_user_edge_file=None,
+    nodes_file,
+    edges_file,
 ):
-    assert user_user_edge_file is None, "Not support User-User relation yet!"
     # load dataset from csv file
-    node_df = pd.read_csv(node_file).drop(['Value', 'Created'], axis=1)
+    node_df = pd.read_csv(nodes_file).drop(['Value', 'Created'], axis=1)
 
     
-    ul_edge_df = pd.read_csv(user_link_edge_file).drop(
-        ['Timestamp', 'Frequency'], axis=1
+    edge_df = pd.read_csv(edges_file).drop(
+        ['Timestamp'], axis=1
     )
-    ul_edge_df.Interaction_Strength = pd.to_numeric(
-        ul_edge_df.Interaction_Strength
-    ).fillna(0)
 
     
     #------------------------------- dataset processing ---------------------------#
 
-    data = {}
-    # split user nodes and post nodes from the node data
-    u_node_df = node_df.loc[node_df.Type == "User"].drop(
-        ['Type', 'Total vote'], axis=1
-    ).fillna(0)
+    data, idx, idx_map = {}, {}, {}
+    node_types = []
     
-    l_node_df = node_df.loc[node_df.Type == "Link", ['ID', 'Total vote', 'Abnormally']]
-    l_node_df['Total vote'] = pd.to_numeric(l_node_df['Total vote']).fillna(0)
-
-    # split edge type
-    uid = u_node_df.ID
-    lid = l_node_df.ID
-    # print(user_id.rename("Source"))
-
-    # map node id to graph node id
-    map_uid = {k:v for v, k in enumerate(uid.to_list())}
-    map_lid = {k:v for v, k in enumerate(lid.to_list())}
-    
-    # user_map_id_reverse = {k:v for k, v in enumerate(user_id.to_list())}
-    # link_map_id_reverse = {k:v for k, v in enumerate(link_id.to_list())}
-    
-    ul_edge_df = ul_edge_df.replace({"Source": map_uid, "Target": map_lid})      
-    
-    data['user'] = {
-        'x': torch.tensor(u_node_df.drop('ID', axis=1).values).float(),
-        'y': torch.tensor(
-            pd.to_numeric(u_node_df['Abnormally'].replace({True:1, False:0})).fillna(0).values
-        ).long()
-    }
-    data['link'] = {
-        'x': torch.tensor(l_node_df.drop('ID', axis=1).values).float(),
-        'y': torch.tensor(
-            pd.to_numeric(l_node_df['Abnormally'].replace({True:1, False:0})).fillna(0).values
-        ).long()
-    }
-
-    
-
-    for type in ul_edge_df.Type.unique():
-        edge_id = np.stack([
-            ul_edge_df.loc[ul_edge_df.Type==type]['Source'].values,
-            ul_edge_df.loc[ul_edge_df.Type==type]['Target'].values
-        ], axis=0)
-        
-        edge_attr = np.stack([
-            ul_edge_df.loc[ul_edge_df.Type==type] ['Interaction_Strength'].values,
-            ul_edge_df.loc[ul_edge_df.Type==type] ['TimeDifference'].values
-        ], axis=1)
-
-        edge_label = pd.to_numeric(ul_edge_df.loc[ul_edge_df.Type==type]['Abnormally'].replace({True:1, False:0})).fillna(0).values
-
-        
-        data['user', type.lower(), 'link']= {
-            'edge_index': torch.from_numpy(edge_id).long(),
-            'edge_attr': torch.from_numpy(edge_attr).float(),
-            'y': torch.from_numpy(edge_label).long()
+    #>>>>>>>>>>>>>>>>>>>>> Extract node features for each node types >>>>>>>>>>>>>>>>>>>>>>
+    for t in node_df.Type.unique():
+        if t == 'User':
+            nodes = node_df.loc[node_df.Type == t].drop(['Type', 'Total vote'], axis=1)
+        elif t in ['Post', 'Comment']:
+            nodes = node_df.loc[node_df.Type == t, ['ID', 'Total vote', 'Abnormally']]
+        else:
+            print("[Warning] This node data has other types than 'User', 'Post' and 'Comment', which is curently not supported and will be ignored while processing!")
+            continue
+        t = t.lower()
+        node_types.append(t)
+        data[t] = {
+            'x': torch.tensor(nodes.drop(['ID', 'Abnormally'], axis=1).values).float(),
+            'y': torch.tensor(pd.to_numeric(nodes['Abnormally'].replace({'True':'1', 'False':'0'})).values)
         }
+        idx[t] = nodes.ID.to_list()
+        idx_map[t] = {k:v for v, k in enumerate(idx[t])}
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Extract edges >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        
+    for t in edge_df.Type.unique():
+        edges = edge_df.loc[edge_df.Type == t].drop('Type', axis=1)
+        if t == "Belong_to":
+            source_target = (
+                ('comment', 'comment'),
+                ('comment', 'post')
+            )
+        else:
+            source_target = (
+                ('user', 'comment'),
+                ('user', 'post'),
+            )
+        for source, target in source_target:
+            edges.loc[edges.Source.isin(idx[source]) & edges.Target.isin(idx[target])]
+            edges.replace({"Source": idx_map[source], "Target": idx_map[target]})
+            edge_idx = np.stack([
+                edges['Source'].values,
+                edges['Target'].values
+            ], axis=0)
+            edge_attr = np.array(np.array(edges.Weight))
+    
+            edge_label = pd.to_numeric(edges['Abnormally'].replace({'True':'1', 'False':'0'})).values
+
+
+            data[source, t.lower(), target]= {
+                'edge_index': torch.from_numpy(edge_idx).long(),
+                'edge_attr': torch.from_numpy(edge_attr).float().view(-1,1),
+                'y': torch.from_numpy(edge_label).long()
+            }
 
     return HeteroData.from_dict(data)
+
+
 
 # def getLinkHiveDataset(
 #     node_file,
